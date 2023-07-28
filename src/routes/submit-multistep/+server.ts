b86a58dac99d0287d69b09072036c73de01dd287
Okan Sahin <okansahinfed@gmail.com>
2023-07-29 00:52:47 +0300

    env import

--- src/routes/submit-multistep/+server.ts
+++ src/routes/submit-multistep/+server.ts
@@ -1,4 +1,4 @@
-import { AIRTABLE_TOKEN, AIRTABLE_TABLE_ID, AIRTABLE_BASE_ID } from '$env/static/private';
+import { AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID, AIRTABLE_TOKEN } from '$env/static/private';
 
 import { json } from '@sveltejs/kit';
 
